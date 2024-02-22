830cd454121b63c236a35f89e67f611c971aa056
Geeradha <geesulakshi@gmail.com>
2024-02-22 19:06:02 +0530

    login and signup page front end

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+    $servername = "localhost";
+    $dbUsername = "root";
+    $Password = "";
+    $db = "cab_service";
+
+    $conn = new mysqli($servername, $dbUsername, $Password,$db);
+    
+    if ($conn->connect_error) {
+       die("Connection failed: " . $conn->connect_error);
+     }
+       echo "Connected successfully";
+     ?>
